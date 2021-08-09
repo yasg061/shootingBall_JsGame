3a43534a37a14dc3e6c95df8c5da274dcd5f101b
yasg061 <kyasming01@gmail.com>
2021-08-09 16:53:03 -0600

    create enemies

--- index.js
+++ index.js
@@ -80,10 +80,21 @@ const enemies = []
 
 function spawnEnemies(){
     setInterval(() => {
-        //console.log('go');
-        const x =   Math.random() * canvas.width
-        const y =   Math.random() * canvas.height
-        const radius = 30
+        const radius = Math.random() * (30 - 4) + 4
+
+        let x
+        let y
+
+        if (Math.random() < 0.5){
+
+            x =   Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y =   Math.random() * canvas.height
+        }else{
+            x =   Math.random() * canvas.width 
+            y =   Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+
         const color = 'green'
         const angle = Math.atan2(
             canvas.height / 2 - y, 
